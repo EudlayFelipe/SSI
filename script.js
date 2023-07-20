de480ecb7b61a727b933bbe57f3de31f2d136752
EudlayFelipe <103539515+EudlayFelipe@users.noreply.github.com>
2023-07-20 15:29:25 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle("light")
+  
+    //  pegar a tag img
+   // const img = document.querySelector("profile img")
+  
+    //  substituir a imagem
+    //  se tiver light mode, adiciona a img light
+    //if (html.classList.contains("light")) {
+    //  img.setAttribute("src", "./assets/EU.png")
+    //}
+    //  se tiver sem light mode, manter a imagem normal
+    //else {
+    //  img.setAttribute("src", "./assets/EU.png")
+    //}
+}
